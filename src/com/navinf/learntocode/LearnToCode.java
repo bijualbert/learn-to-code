a909cdb9f42ca9ca15e641880ea27d63443ca105
Navin <navin.kurupacheril@gmail.com>
2013-11-09 15:56:16 -0800

    .

---
+++ src/com/navinf/learntocode/LearnToCode.java
@@ -0,0 +1,43 @@
+package com.navinf.learntocode;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LearnToCode {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LearnToCode window = new LearnToCode();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LearnToCode() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
