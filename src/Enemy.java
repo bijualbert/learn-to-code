cd88ebe3c65939f2c688f266c9a53d5fc0642ffb
joshuakpearce <joshuakpearce@gmail.com>
2013-11-09 17:28:54 -0800

    Enemy changes

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,13 +1,48 @@
 
 public class Enemy extends Character
 {
-	public Enemy( int x, int y, int width, int height, int health, boolean moving )
+	public enum Facing { LEFT, RIGHT }
+	Facing face = Facing.LEFT;
+	
+	public Enemy( int x, int y, int width, int height, int health, boolean mobile )
 	{
-		super( x, y, width, height, health, moving );
+		super( x, y, width, height, health, mobile );
 	}
 	
-	public void movement()
+	public void movement(Player player)
+	{
+	
+		while()
+		{
+			switch( face )
+			{
+			case LEFT:
+			{
+			
+				if( player.getX() < getX() && getX() - player.getX() <= 200 )
+					shoot();
+		 	}
+		 		break;
+		 		
+		 	case RIGHT:
+		 	{
+		 		if( player.getX() > getX() && player.getX() - getX() <= 200 )
+					shoot();
+			}
+				break;
+				
+			default:
+				break;
+			}
+		}
+	}
+	
+	public void patrol(  )
 	{
 		
 	}
+	
+	public void shoot()
+	{}
+	
 }
