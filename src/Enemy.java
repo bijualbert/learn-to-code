78a1eeb7492665eb635bee4c24fed216990c87c5
joshuakpearce <joshuakpearce@gmail.com>
2013-11-09 18:04:01 -0800

    more enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,49 +1,71 @@
 import java.awt.*;
+import java.util.*;
 
 public class Enemy extends Character
 {
 	public enum Facing { LEFT, RIGHT }
 	Facing face = Facing.LEFT;
+	Random random = new Random();
 	
 	public Enemy( int x, int y, int health )
 	{
 		super( x, y, health );
 	}
 	
-	public void movement(Player player)
+	public void movement()
 	{
-	
-		while()
+		int choice = random.nextInt( 3 ) + 1;
+		something( player );
+		
+		switch( choice )
 		{
-			switch( face )
-			{
-			case LEFT:
-			{
+		case 1:
+			if( face == Facing.LEFT )
+				moveLeft();
+			else
+				moveRight();
+			break;
 			
-				if( player.getX() < getX() && getX() - player.getX() <= 200 )
-					shoot();
-		 	}
-		 		break;
+		case 2:
+			if( face == Facing.LEFT )
+				face = Facing.RIGHT;
+			else
+				face = Facing.LEFT;
+			break;
+			
+		case 3:
+			break;
+			
+		default:
+			break;
+		}
+	}
+	
+	private void something( Player player )
+	{
+		switch( face )
+		{
+		case LEFT:
+		{	
+			if( player.getX() < getX() && getX() - player.getX() <= 200 )
+				shoot();
+		}
+			break;
 		 		
-		 	case RIGHT:
-		 	{
-		 		if( player.getX() > getX() && player.getX() - getX() <= 200 )
-					shoot();
-			}
-				break;
+		case RIGHT:
+		 {
+		 	if( player.getX() > getX() && player.getX() - getX() <= 200 )
+				shoot();
+		}
+			break;
 				
-			default:
-				break;
-			}
+		default:
+			break;
 		}
 	}
 
-	public void shoot()
+	private void shoot()
 	{}
-
-	public void draw(Graphics g){
-		
-	}
 	
 	public void update(){
 		
