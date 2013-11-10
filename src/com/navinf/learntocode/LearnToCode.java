4c141cb236911432a72bb11c54341855ea04d6ac
Navin <navin.kurupacheril@gmail.com>
2013-11-09 17:28:55 -0800

    added localization files for java editor

--- src/com/navinf/learntocode/LearnToCode.java
+++ src/com/navinf/learntocode/LearnToCode.java
@@ -1,29 +1,49 @@
 package com.navinf.learntocode;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.tools.JavaCompiler;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
+
 import org.fife.ui.rtextarea.*;
 import org.fife.ui.rsyntaxtextarea.*;
 
 public class LearnToCode {
+	private static volatile LearnToCode instance;
 
 	private JFrame frame;
+	private RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
 
 	/**
 	 * Launch the application.
 	 */
+	@SuppressWarnings("serial")
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					LearnToCode window = new LearnToCode();
 					window.frame.setVisible(true);
+					LearnToCode.instance=window;
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+		
+		JavaCompiler jc = ToolProvider.getSystemJavaCompiler();
+		
+		while(LearnToCode.instance==null);
+		while(true){
+			final String code = LearnToCode.instance.textArea.getText();
+			ArrayList<SimpleJavaFileObject> java_files = new ArrayList<SimpleJavaFileObject>(){{
+				add(new JavaSourceFromString("Main",code));
+			}};
+			jc.getTask(null, null, null, null, null, java_files);
+		}
 	}
 
 	/**
@@ -41,7 +61,6 @@ public class LearnToCode {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
 		textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
 		textArea.setCodeFoldingEnabled(true);
 		RTextScrollPane sp = new RTextScrollPane(textArea);
