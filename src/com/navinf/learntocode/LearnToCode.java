4b866b35914e2e67100afac80cd05b6daf2900d5
Navin <navin.kurupacheril@gmail.com>
2013-11-10 06:03:51 -0800

    .

--- src/com/navinf/learntocode/LearnToCode.java
+++ src/com/navinf/learntocode/LearnToCode.java
@@ -64,7 +64,7 @@ public class LearnToCode {
 		boolean ran = false;
 		while(true){
 			final String code = "import java.util.*;\n"+
-								"import static GamePanel.*;\n"+
+								"import static com.navinf.learntocode.GamePanel.*;\n"+
 								"public class PlayerCodeImpl extends com.navinf.learntocode.PlayerCode{"+
 								"public Mobile player = (Mobile)GamePanel.me.player;\n"+
 								textArea.getText()+
